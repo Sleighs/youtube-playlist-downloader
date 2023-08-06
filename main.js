db4ebce53be7ef59aed09d5a27a6dd66c7e70b42
Samuel Wright <wsright987@gmail.com>
2023-08-05 21:16:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,17 +12,23 @@ const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
 let locations = JSON.parse(fs.readFileSync('./locations.json', 'utf-8'))
 
 function timeout(miliseconds) {
-    return new Promise(resolve => setTimeout(resolve, miliseconds));
+  return new Promise(resolve => setTimeout(resolve, miliseconds));
 } 
 
 const setupBrowser = async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1024, height: 1080 });
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1024, height: 1080 });
 
   return [browser, page]
-
-
 }
 
-console.log(setupBrowser)
+const run = async () => {
+  const [browser, page] = await setupBrowser()
+
+  console.log("Logging in...")
+  await page.goto("https://www.samuelwright.dev/");
+  
+}
+
+run()
