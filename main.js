951e17267afdcdd131dc4caead0f4fcda1c26aa1
Samuel Wright <wsright987@gmail.com>
2023-08-06 07:57:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ https://www.npmjs.com/package/youtube-dl
 
 https://blog.apify.com/puppeteer-web-scraping-tutorial/
 
+https://intoli.com/blog/scrape-infinite-scroll/
+
 */
 
 const fs = require("fs");
 const puppeteer = require("puppeteer");
+
+const playlist = 'https://www.youtube.com/playlist?list=PL8xvCGHIJPU__F_pFHbYujfin-vyRIJXu';//"https://www.youtube.com/playlist?list=PL8xvCGHIJPU8z9itQiCZbAjQK_V2SPKIn";
 
 // const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
 
@@ -45,22 +49,19 @@ const run = async () => {
 
   console.log("Logging in...");
 
-  await page.goto("https://samuelwright.dev/");
+  await page.goto(playlist);
 
-  await page.waitForSelector('.projects-row-title');
+  await page.waitForSelector('ytd-playlist-video-renderer');
+
+  
   
   await page.evaluate(() => {
-    const projects = Array.from(document.querySelectorAll('.projects-row-title'));
+    const playlistItems = Array.from(document.querySelectorAll('ytd-playlist-video-renderer'));
 
-    return console.log('projects', projects[0]);
+    return console.log('playlist items', playlistItems);
   });
 
-  // await page.evaluate(() => console.log('hello', 5));
-  // await page.evaluate(() => console.log({ foo: 'bar' }));
-  // await page.evaluate(() => console.log([1, 2, 3, 4, 5]));
-
-
-    // Pause for 10 seconds, to see what's going on.
+  // Pause for 10 seconds, to see what's going on.
   await new Promise(r => setTimeout(r, 15000));
 
   // Turn off the browser to clean up after ourselves.
